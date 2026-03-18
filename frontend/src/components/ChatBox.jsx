9ba97829d458ddad07f0c39fd9ac9eb9bdebf3f5
Sanket Kadam <sk8185710@gmail.com>
2026-03-18 11:58:54 +0530

    add updated fronted to check deployment

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -29,7 +29,7 @@ function ChatBox({ selectedDoc }) {
 
     setLoading(true);
     try {
-      const res = await axios.post("http://13.234.168.225/ask", {
+      const res = await axios.post("http://13.234.168.225:5000/ask", {
         question: input,
         document: selectedDoc || null,
       });
